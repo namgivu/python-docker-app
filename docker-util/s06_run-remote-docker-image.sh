83a8f861245bc8e31b150234ccc42b9e71387c9b
namgivu <namgivu@gmail.com>
2017-05-03 19:12:41 +0700

    run remote image @ docker util

---
+++ docker-util/s06_run-remote-docker-image.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
+
+#load util config
+source "$SCRIPT_HOME/s00_config.sh"
+
+#build image from app at $APP_HOME
+docker run -d -p $RUN_PORT:$IMAGE_PORT $IMAGE_TAG_REMOTE
