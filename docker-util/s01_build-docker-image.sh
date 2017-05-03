6ac795979f531299c2fda833eb0d9d60b9cfb1a8
namgivu <namgivu@gmail.com>
2017-05-03 18:17:05 +0700

    rename config @ docker util

--- docker-util/s01_build-docker-image.sh
+++ docker-util/s01_build-docker-image.sh
@@ -4,7 +4,7 @@
 s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
 
 #load util config
-source "$SCRIPT_HOME/config.sh"
+source "$SCRIPT_HOME/s00_config.sh"
 
 #build image from app at $APP_HOME
 docker build -t $IMAGE_TAG $APP_HOME
