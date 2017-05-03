62ab510e359c3ab341a4a2257dd9de794782aa42
namgivu <namgivu@gmail.com>
2017-05-03 19:16:22 +0700

    run w/ detach param @ docker util

--- docker-util/s06_run-remote-docker-image.sh
+++ docker-util/s06_run-remote-docker-image.sh
@@ -6,5 +6,8 @@ s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
 #load util config
 source "$SCRIPT_HOME/s00_config.sh"
 
+#load input
+DETACH_MODE=$1 #e.g. pass detach param as -d
+
 #build image from app at $APP_HOME
-docker run -d -p $RUN_PORT:$IMAGE_PORT $IMAGE_TAG_REMOTE
+docker run $DETACH_MODE -p $RUN_PORT:$IMAGE_PORT $IMAGE_TAG_REMOTE
