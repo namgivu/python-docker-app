50f062c2de825609db67f590b23266c24d15ba62
namgivu <namgivu@gmail.com>
2017-05-03 18:32:38 +0700

    list docker container @ docker util

--- docker-util/s00_config.sh
+++ docker-util/s00_config.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
-#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
-s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ;
-SCRIPT_HOME=$s
+##region common bash util
+  #get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+  s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ;
+  SCRIPT_HOME=$s
 
-#app home
-s="$SCRIPT_HOME/.." ; s=$(cd "$s" && pwd) ;
-APP_HOME=$s
+  #app home
+  s="$SCRIPT_HOME/.." ; s=$(cd "$s" && pwd) ;
+  APP_HOME=$s
 
-#docker image name/tag for the app
-IMAGE_TAG='friendlyhello'
+  #coloring
+  HL='\033[1;33m' #high-lighted color
+  CM='\033[0;32m' #comment color
+  ER='\033[1;31m' #error color
+  EC='\033[0m'    #end coloring
 
-#running port setup
-RUN_PORT=5000
-IMAGE_PORT=80 #TODO extract this port from Dockerfile
+##endregion common bash util
+
+
+##region app specific config
+  #docker image name/tag for the app
+  IMAGE_TAG='friendlyhello'
+
+  #running port setup
+  RUN_PORT=5000
+  IMAGE_PORT=80 #TODO extract this port from Dockerfile
+##endregion app specific config
---
+++ docker-util/s03_list-docker-container.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
+
+#load util config
+source "$SCRIPT_HOME/s00_config.sh"
+
+#list running docker-image i.e. docker-container
+echo -e "${CM}All container${EC}"
+docker ps
+
+echo
+
+echo -e "${CM}The app container${EC}"
+docker ps | grep $IMAGE_TAG
