c323c54d7c054e9ed5bc8c3653787f9614f736df
namgivu <namgivu@gmail.com>
2017-05-05 16:00:58 +0700

    list info @ docker stack

--- docker-util/s06b_replica-list.sh
+++ docker-util/s06b_replica-list.sh
@@ -7,5 +7,23 @@ s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
 source "$SCRIPT_HOME/s00_config.sh"
 
 
-#view running replicas
+#do listing
+echo
+
+echo -e "${CM}All stack${EC}"
+docker stack ls
+
+echo
+
+echo -e "${CM}This app's stack${EC}"
+docker stack ls | grep $IMAGE_STACK_NAME
+
+echo
+
+echo -e "${CM}Stack's services${EC}"
+docker stack services $IMAGE_STACK_NAME
+
+echo
+
+echo -e "${CM}Stack's services breakdown i.e. listing its containers${EC}"
 docker stack ps $IMAGE_STACK_NAME
