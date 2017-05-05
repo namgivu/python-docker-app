a8dd71a09185d57cc1e8bfcc782d71e7cea8a990
namgivu <namgivu@gmail.com>
2017-05-05 13:45:09 +0700

    upload image

---
+++ docker-util/s04_image-upload.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
+
+#load util config
+source "$SCRIPT_HOME/s00_config.sh"
+
+
+#region push local docker-image to remote docker cloud
+  echo -e "${CM}Name/tag the remote image${EC}"
+  sh="docker tag $IMAGE_NAME_LOCAL $IMAGE_NAME_REMOTE"
+  echo $sh ; eval $sh
+
+  echo
+
+  echo -e "${CM}Push image to remote${EC}"
+  sh="docker push $IMAGE_NAME_REMOTE"
+  echo $sh ; eval $sh
+
+#endregion push local docker-image to remote docker cloud
