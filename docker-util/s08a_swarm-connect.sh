71a5d3ec553c0df164afc5b0ac9d1f1eb0ab41e5
namgivu <namgivu@gmail.com>
2017-05-09 13:42:36 +0700

    remote swarm 0th

---
+++ docker-util/s08a_swarm-connect.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
+
+#load util config
+source "$SCRIPT_HOME/s00_config.sh"
+
+
+#connect to remote Docker Swarm
+docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_HOST dockercloud/client $REMOTE_SWARM_NAME
+
+#you will need to run below command yourself
+#export DOCKER_HOST=tcp://127.0.0.1:32768
+
---
+++ docker-util/s08b_swarm-deploy-stack.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
+
+#load util config
+source "$SCRIPT_HOME/s00_config.sh"
+
+
+#do deploy
+docker stack deploy -c docker-compose.yml $IMAGE_STACK_NAME
