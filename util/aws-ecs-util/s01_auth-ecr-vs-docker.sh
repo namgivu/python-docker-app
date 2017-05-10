5194300e4789850cdcf6c1981fc81dff2a5dcb51
namgivu <namgivu@gmail.com>
2017-05-10 15:15:21 +0700

    aws ecr vs docker auth

---
+++ util/aws-ecs-util/s01_auth-ecr-vs-docker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+##region common bash util
+  #get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
+  s=${BASH_SOURCE} ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ;
+  SCRIPT_HOME=$s
+
+  #import common setting
+  source "$SCRIPT_HOME/s00_config.sh"
+
+##endregion common bash util
+
+
+#print docker login command
+aws ecr get-login
+
+#print guide
+echo -e "
+${CM}Please run above command as guided to proceed login docker using AWS account${EC}
+"
