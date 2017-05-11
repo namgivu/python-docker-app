166e551ca948ee53ad8c549a547aacaf831bfedb
namgivu <namgivu@gmail.com>
2017-05-11 15:44:29 +0700

    AWS_DEFAULT_REGION @ config

--- util/aws-ecs-util/s00_config.local-TEMPLATE.sh
+++ util/aws-ecs-util/s00_config.local-TEMPLATE.sh
@@ -4,3 +4,4 @@ AWS_ACCOUNT_ID='your.AWS_ACCOUNT_ID'
 
 AWS_ACCESS_KEY_ID='your.AWS_ACCESS_KEY_ID'
 AWS_ACCESS_KEY_SECRET='your.AWS_ACCESS_KEY_SECRET'
+AWS_DEFAULT_REGION='ap-southeast-1' #you can leave it empty ie. us-east-1
